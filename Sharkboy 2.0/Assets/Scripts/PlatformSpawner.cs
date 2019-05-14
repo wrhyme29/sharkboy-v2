7381822d64742d12dfdd8b51e8f6c0d7c8f28b91
wrhyme29 <joshuafields2019@u.northwestern.edu>
2019-05-14 11:01:03 -0500

    Added Game Files

---
+++ Sharkboy 2.0/Assets/Scripts/PlatformSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner
+{
+	ObjectPooler objectPooler;
+
+	private void Start(){
+		objectPooler = ObjectPooler.Instance;
+	}
+
+    public GameObject spawnPlatform(Vector3 position, Quaternion rotation){
+		GameObject platform = objectPooler.SpawnFromPool("Platform", position, rotation);
+		return platform;
+	}
+}
