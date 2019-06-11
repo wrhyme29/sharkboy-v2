4cbc5b114f50d9b4e824c70d2cdd5d16fa656b18
wrhyme29 <joshuafields2019@u.northwestern.edu>
2019-06-10 21:05:09 -0500

    Added UI for Coins

--- Sharkboy 2.0/Assets/Scripts/PickupCoin.cs
+++ Sharkboy 2.0/Assets/Scripts/PickupCoin.cs
@@ -1,17 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PickupCoin : MonoBehaviour
 {
 	GameStats gs;
+	public Text​Mesh​Pro​UGUI text;
+	bool locked = false;
 
 	void Start(){
 		gs = GameStats.Instance;
+		text.text = "Coins: " + gs.coins;
+
 	}
     void OnTriggerEnter2D(Collider2D col)
     {
-		gs.coins++;
-		Destroy(this.gameObject);
+		if(!locked){
+			locked = true;
+			gs.coins++;
+			UpdateUI();
+			Destroy(this.gameObject);
+		}
+
     }
+
+	void UpdateUI(){
+		text.text = "Coins: " + gs.coins;
+	}
 }
