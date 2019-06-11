72d52aa806a16cd5b9db00346297916bf654898b
wrhyme29 <joshuafields2019@u.northwestern.edu>
2019-06-10 20:16:57 -0500

    Added Coin Prefab and GameManager
    Pickup of coin is attached to the coin prefab, number of coins is stored in singleton of gamestats attached to game manager

---
+++ Sharkboy 2.0/Assets/Scripts/GameStats.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStats : MonoBehaviour
+{
+
+	#region Singleton
+	public static GameStats Instance;   
+
+	//Awake is always called before any Start functions
+	void Awake()
+	{
+		Instance = this;
+	}
+
+	#endregion
+	
+   public int coins = 0;
+}
---
+++ Sharkboy 2.0/Assets/Scripts/PickupCoin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupCoin : MonoBehaviour
+{
+	GameStats gs;
+
+	void Start(){
+		gs = GameStats.Instance;
+	}
+    void OnTriggerEnter2D(Collider2D col)
+    {
+		gs.coins++;
+		Destroy(this.gameObject);
+    }
+}
